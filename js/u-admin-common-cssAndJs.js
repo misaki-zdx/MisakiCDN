36b8e9871987b102e417c1f887984c6a673587ce
Misaki <1628666074@qq.com>
2021-04-01 11:41:11 +0800

    新

---
+++ js/u-admin-common-cssAndJs.js
@@ -0,0 +1,35 @@
+/*css部分*/
+//bootStrap4.2.1
+document.write('<link rel="stylesheet" href="https://ajax.aspnetcdn.com/ajax/bootstrap/4.2.1/css/bootstrap.min.css">');
+//font-awesome/5.8.2
+document.write('<link rel=\'stylesheet\' href=\'https://cdn.staticfile.org/font-awesome/5.8.2/css/all.min.css\'>')
+//ionicons
+document.write('<link rel=\'stylesheet\' href=\'https://cdn.staticfile.org/ionicons/2.0.0/css/ionicons.min.css\'>')
+document.write('<link rel=\'stylesheet\' href=\'https://cdn.staticfile.org/summernote/0.8.9/summernote-lite.css\'>')
+document.write('<link rel=\'stylesheet\' href=\'https://cdn.staticfile.org/flag-icon-css/3.5.0/css/flag-icon.min.css\'>')
+
+const cdn_path = "https://cdn.jsdelivr.net/gh/misaki-zdx/misakiCdn";
+document.write('<link rel="stylesheet" href=' + cdn_path + '/css/demo.css>')
+document.write('<link rel="stylesheet" href=' + cdn_path + '/css/style.css>')
+document.write('<link rel="stylesheet" href=' + cdn_path + '/plug-ins/xcConfirm/css/xcConfirm.css>')
+document.write('<link rel="stylesheet" href=' + cdn_path + '/plug-ins/context-menu/css/contextMenu.css>')
+document.write('<link rel="stylesheet" href=' + cdn_path + '/css/upload-file.css>')
+
+/*js部分*/
+document.write('<script src="https://cdn.staticfile.org/jquery/3.6.0/jquery.min.js"></script>')
+document.write('<script src="https://cdn.staticfile.org/summernote/0.8.9/summernote-lite.min.js"></script>')
+document.write('<script src="https://cdn.staticfile.org/twitter-bootstrap/4.2.1/js/bootstrap.min.js"></script>')
+document.write('<script src=' + cdn_path + '/modules/popper.js>')
+document.write('<script src="https://cdn.staticfile.org/tooltip.js/1.3.3/tooltip.min.js"></script>')
+document.write('<script src=' + cdn_path + '/modules/nicescroll/jquery.nicescroll.min.js></script>')
+
+document.write('<script src=' + cdn_path + '/modules/scroll-up-bar/dist/scroll-up-bar.min.js></script>')
+document.write('<script src=' + cdn_path + '/js/sa-functions.js></script>')
+document.write('<script src="https://cdn.staticfile.org/Chart.js/2.9.4/Chart.min.js"></script>')
+document.write('<script src=' + cdn_path + '/plug-ins/context-menu/js/jquery.contextMenu.min.js></script>')
+document.write('<script src=' + cdn_path + '/plug-ins/xcConfirm/js/xcConfirm.js></script>')
+document.write('<script src=' + cdn_path + '/js/scripts.js></script>')
+document.write('<script src=' + cdn_path + '/js/custom.js></script>')
+document.write('<script src=' + cdn_path + '/js/demo.js></script>')
+document.write('<script src=' + cdn_path + '/js/easyUploader.jq.js></script>')
+
